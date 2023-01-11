2e47c7f1dab4a657b8840845c3c92ae09cc151e1
brijesh desai <110248235+brijeshdesai666@users.noreply.github.com>
2023-01-11 23:19:42 +0530

    Create Celender_All_Month.c

---
+++ Celender_All_Month.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+# include <stdlib.h>
+
+int main() {
+    int year, month;
+    char *month_name[12] = {"January", "February", "April", "April","may","june","july", "auguest",  "September", "october","November","december"};
+    printf("Enter the year: ");
+    scanf("%d", &year);
+    // printf("Enter the month (1-12): ");
+    // scanf("%d", &month);
+    // if (month > 12)
+    // {
+    //     printf("Ivalid month!!!\n");
+    //     exit(0);
+    // }
+    
+
+    for (int i = 1; i <= 12; i++)
+    {
+    
+    // Get the first day of the month
+    struct tm t = {0};
+    t.tm_year = year - 1900;
+    t.tm_mon = i - 1;
+    t.tm_mday = 1;
+    mktime(&t);
+
+    // Print the calendar
+    printf("    %s %d\n",month_name[i-1],  year);
+    printf("Su Mo Tu We Th Fr Sa\n");
+    for (int i = 0; i < t.tm_wday; i++) {
+        printf("   ");
+    }
+    for (int i = 1; i <= 31; i++) {
+        if (t.tm_mday != i) {
+            break;
+        }
+        printf("%2d ", i);
+        if (t.tm_wday == 6) {
+            printf("\n");
+        }
+        t.tm_mday++;
+        mktime(&t);
+    }
+    printf("\n");
+    }
+
+    return 0;
+}
